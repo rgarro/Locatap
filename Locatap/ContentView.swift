41abf7846ef25c91b7116e88cd145e616c305034
rgarro <rgarro@gmail.com>
2023-12-05 21:14:00 -0600

    Initial Commit

---
+++ Locatap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Locatap
+//
+//  Created by Rolando Garro on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Locatap/LocatapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocatapApp.swift
+//  Locatap
+//
+//  Created by Rolando Garro on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LocatapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
