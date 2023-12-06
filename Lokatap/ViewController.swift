1758198b8b74c4f668bdfb65d51c7ab5dd2b72bb
rgarro <rgarro@gmail.com>
2023-12-05 21:32:22 -0600

    first commit

---
+++ Lokatap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lokatap
+//
+//  Created by Rolando Garro on 12/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
