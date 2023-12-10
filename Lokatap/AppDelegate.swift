1f33ec709530ae5db213709c2aaa0a12281ab14f
rgarro <rgarro@gmail.com>
2023-12-10 16:19:14 -0600

    blame it on the rain ...

--- Lokatap/AppDelegate.swift
+++ Lokatap/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var myVar: String = ""//Blame it on the rain ..
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
