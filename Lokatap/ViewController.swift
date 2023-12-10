d7a6e51d1e1545dcfddaa4c21a02a7035b1ea1a7
rgarro <rgarro@gmail.com>
2023-12-10 16:53:28 -0600

    blame it on the rain II...

--- Lokatap/ViewController.swift
+++ Lokatap/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("here we go")
+    }
+    
 }
 
